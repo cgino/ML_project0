e7aaefa39806645012ab9e42752a1d8afe571750
Gino <cgino@ethz.ch>
2020-03-04 15:02:14 +0100

    csv imprt running

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # main file
 
+from sklearn.metrics import mean_squared_error
+
+import csv
+import numpy as np
+
+train = []
+with open('task0_sl19d1/train.csv', 'r') as file:
+    reader = csv.reader(file)
+    #train = np.asarray(reader)
+    for row in reader:
+        print(row)
+
+print(len(train))
+#RMSE = mean_squared_error(y, y_pred)**0.5
+print(train)
