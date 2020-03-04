254949ac4d4315ae5f6d270db95efc07da35a3e6
Gino <cgino@ethz.ch>
2020-03-04 16:19:03 +0100

    building lin reg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main file
 
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 
 import csv
 import numpy as np
@@ -10,8 +11,21 @@ with open('task0_sl19d1/train.csv', 'r') as file:
     reader = csv.reader(file)
     #train = np.asarray(reader)
     for row in reader:
-        print(row)
+        train.append(row)
 
 print(len(train))
+print(train[0])
+
+train_data = np.asarray(train)
+print(train_data.shape)
+y = train_data[:,1]
+X = train_data[:,2:]
+
+reg = LinearRegression().fit(X[1:,:], y[1:])
+
+print(y[0:5])
+print(X[0])
+print(reg.score)
+
 #RMSE = mean_squared_error(y, y_pred)**0.5
-print(train)
+#print(train)
